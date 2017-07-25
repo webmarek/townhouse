ed5e69de3f0ad0e0eb2becf5016064cf096f193c
webmarek <marek1990kaminski@gmail.com>
2017-07-25 10:28:36 +0200

    added an Immediately Invoked Function Expression in index.js file in js/home

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -1,41 +1,44 @@
 /* this is home js*/
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	//settings for slider
-	var width = 720;
-	var animationSpeed = 1000;
-	var pause = 3000;
-	var currentSlide = 1;
+	//putting that one into a Immediately Invoked Function Expression
+	(function () {
+		//settings for slider
+		var width = 720;
+		var animationSpeed = 1000;
+		var pause = 3000;
+		var currentSlide = 1;
 
-	//cache DOM elements
-	var $slider = $('#slider');
-	var $slideContainer = $slider.find('.slides');//its one piece
-	var $slides = $slideContainer.find('.slide');//it is an array
+		//cache DOM elements
+		var $slider = $('#slider');
+		var $slideContainer = $slider.find('.slides');//its one piece
+		var $slides = $slideContainer.find('.slide');//it is an array
 
-	var interval;
+		var interval;
 
-	function startSlider() {
-		interval = setInterval(function() {
-			$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-				if (++currentSlide === $slides.length) {
-					currentSlide = 1;
-					$slideContainer.css('margin-left', 0);
-				}
-			});
-		}, pause);
-	}
+		function startSlider() {
+			interval = setInterval(function () {
+				$slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function () {
+					if (++currentSlide === $slides.length) {
+						currentSlide = 1;
+						$slideContainer.css('margin-left', 0);
+					}
+				});
+			}, pause);
+		}
 
-	function pauseSlider() {
-		clearInterval(interval);
-	}
+		function pauseSlider() {
+			clearInterval(interval);
+		}
 
-	$slideContainer
-		.on('mouseenter', pauseSlider)
-		.on('mouseleave', startSlider);
+		$slideContainer
+			.on('mouseenter', pauseSlider)
+			.on('mouseleave', startSlider);
 
-	startSlider();
+		startSlider();
 
+	})();
 
 });
